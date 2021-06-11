1ef2f0f10dfb0af1bc2baca7d57a04c1d2a2d9ef
arunaofdawn <85207407+arunaofdawn@users.noreply.github.com>
2021-06-11 13:54:41 +0700

    Update feds.py

--- DaisyX/modules/feds.py
+++ DaisyX/modules/feds.py
@@ -1369,36 +1369,36 @@ async def __import__(chat_id, data):
 __mod_name__ = "Federations"
 
 __help__ = """
-Well basically there is 2 reasons to use Federations:
-1. You have many chats and want to ban users in all of them with 1 command
-2. You want to subscribe to any of the antispam Federations to have your chat(s) protected.
-In both cases Daisy will help you.
-<b>Arguments types help:</b>
-<code>()</code>: required argument
-<code>(user)</code>: required but you can reply on any user's message instead
-<code>(file)</code>: required file, if file isn't provided you will be entered in file state, this means Daisy will wait file message from you. Type /cancel to leave from it.
-<code>(? )</code>: additional argument
-<b>Only Federation owner:</b>
-- /fnew (name) or /newfed (name): Creates a new Federation
-- /frename (?Fed ID) (new name): Renames your federation
-- /fdel (?Fed ID) or /delfed (?Fed ID): Removes your Federation
-- /fpromote (user) (?Fed ID): Promotes a user to the your Federation
-- /fdemote (user) (?Fed ID): Demotes a user from the your Federation
-- /fsub (Fed ID): Subscibes your Federation over provided
-- /funsub (Fed ID): unsubscibes your Federation from provided
-- /fsetlog (? Fed ID) (? chat/channel id) or /setfedlog (? Fed ID) (? chat/channel id): Set's a log chat/channel for your Federation
-- /funsetlog (?Fed ID) or /unsetfedlog (?Fed ID): Unsets a Federation log chat\channel
-- /fexport (?Fed ID): Exports Federation bans
-- /fimport (?Fed ID) (file): Imports Federation bans
-<b>Only Chat owner:</b>
-- /fjoin (Fed ID) or /joinfed (Fed ID): Joins current chat to provided Federation
-- /fleave or /leavefed: Leaves current chat from the fed
-<b>Avaible for Federation admins and owners:</b>
-- /fchatlist (?Fed ID) or /fchats (?Fed ID): Shows a list of chats in the your Federation list
-- /fban (user) (?Fed ID) (?reason): Bans user in the Fed and Feds which subscribed on this Fed
-- /sfban (user) (?Fed ID) (?reason): As above, but silently - means the messages about fbanning and replied message (if was provided) will be removed
-- /unfban (user) (?Fed ID) (?reason): Unbans a user from a Federation
-<b>Avaible for all users:</b>
-- /fcheck (?user): Check user's federation ban info
-- /finfo (?Fed ID): Info about Federation
+Yah pada dasarnya ada 2 alasan untuk menggunakan Federasi:
+1. Anda memiliki banyak obrolan dan ingin melarang semua pengguna dengan 1 perintah
+2. Anda ingin berlangganan Federasi antispam mana pun agar obrolan Anda terlindungi.
+Dalam kedua kasus, Bot akan membantu Anda.
+<b>Jenis argumen membantu:</b>
+<code>()</code>: argumen diperlukan
+<code>(user)</code>: diperlukan tetapi Anda dapat membalas pesan pengguna mana pun sebagai gantinya
+<code>(file)</code>: file dibutuhkan, jika file tidak disediakan, Anda akan dimasukkan dalam status file, ini berarti Bot akan menunggu file pesan dari Anda. Ketik /cancel untuk keluar darinya.
+<code>(? )</code>: argumen tambahan
+<b>Khusus pemilik Federasi:</b>
+- /fnew (nama) atau /newfed (nama): Membuat Federasi baru
+- /frename (?Fed ID) (new name): Ganti nama federasi Anda
+- /fdel (?Fed ID) atau /delfed (?Fed ID): Menghapus Federasi Anda
+- /fpromote (user) (?Fed ID): Mempromosikan pengguna ke Federasi Anda
+- /fdemote (user) (?Fed ID): Menurunkan pengguna dari Federasi Anda your
+- /fsub (Fed ID): Berlangganan Federasi Anda lebih dari yang disediakan
+- /funsub (Fed ID): berhenti berlangganan Federasi Anda dari yang disediakan
+- /fsetlog (? Fed ID) (? chat/channel id) atau /setfedlog (? Fed ID) (? chat/channel id): Setel grup/chanel log untuk Federasi Anda
+- /funsetlog (?Fed ID) atau /unsetfedlog (?Fed ID): Menghapus obrolan log Federasi\chanel
+- /fexport (?Fed ID): Larangan Federasi Ekspor
+- /fimport (?Fed ID) (file): Larangan Federasi Impor
+<b>Khusus pemilik Grup:</b>
+- /fjoin (Fed ID) atau /joinfed (Fed ID): Bergabung dengan obrolan saat ini ke Federasi yang disediakan
+- /fleave or /leavefed: Meninggalkan obrolan saat ini dari fed
+<b>Tersedia untuk admin dan pemilik Federasi:</b>
+- /fchatlist (?Fed ID) atau /fchats (?Fed ID): Shows a list of chats in the your Federation list
+- /fban (user) (?Fed ID) (?reason): Bans user di Fed dan Fed yang berlangganan Fed ini
+- /sfban (user) (?Fed ID) (?reason): Seperti di atas, tapi secara diam-diam - berarti pesan tentang fbanning dan pesan balasan (jika diberikan) akan dihapus
+- /unfban (user) (?Fed ID) (?reason): Membatalkan larangan pengguna dari Federasi
+<b>Tersedia untuk semua pengguna:</b>
+- /fcheck (?user): Periksa info larangan federasi pengguna
+- /finfo (?Fed ID): Info tentang Federasi
 """
