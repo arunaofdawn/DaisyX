ec31a11f1744a1cbc8508a5561492e2d56c032a7
arunaofdawn <85207407+arunaofdawn@users.noreply.github.com>
2021-06-12 11:14:12 +0700

    Update imgeditor.py

--- DaisyX/modules/imgeditor.py
+++ DaisyX/modules/imgeditor.py
@@ -450,7 +450,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             await removebg_sticker(client, query.message)
 
 
-__mod_name__ = "Image Editor"
+__mod_name__ = "🎨Edit Foto"
 __help__ = """
 <b> IMAGE EDITOR </b>
 Roso memiliki beberapa alat pengeditan gambar canggih bawaan
