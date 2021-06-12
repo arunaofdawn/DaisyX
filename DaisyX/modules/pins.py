4e434f744842fc8e96e8efe2e37248665c21dbca
arunaofdawn <85207407+arunaofdawn@users.noreply.github.com>
2021-06-12 11:49:56 +0700

    Update pins.py

--- DaisyX/modules/pins.py
+++ DaisyX/modules/pins.py
@@ -58,7 +58,7 @@ async def pin_message(message, strings):
         await message.reply(strings["chat_not_modified_pin"])
 
 
-__mod_name__ = "Pinning"
+__mod_name__ = "📌Pinning"
 
 __help__ = """
 Semua perintah terkait pin dapat ditemukan di sini; tetap perbarui obrolan Anda tentang berita terbaru dengan pesan yang disematkan secara sederhana!
