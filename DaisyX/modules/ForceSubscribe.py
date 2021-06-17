39950e110cf637b48aefa5bb96007b5d0c4eed72
arunaofdawn <85207407+arunaofdawn@users.noreply.github.com>
2021-06-17 21:09:03 +0700

    Update ForceSubscribe.py

--- DaisyX/modules/ForceSubscribe.py
+++ DaisyX/modules/ForceSubscribe.py
@@ -230,4 +230,4 @@ Catatan: Hanya pembuat grup yang dapat mengatur saya dan saya tidak akan mengizi
 Catatan: /forcesub adalah alias dari /forcesubscribe
  
 """
-__mod_name__ = "🔔Force Subs "
+__mod_name__ = "Force Subs🔔 "
